c487b27dec2fc24c4ff710e6852a2e5610797407
okadareo <c0a2203640@edu.teu.ac.jp>
2023-07-04 17:41:27 +0900

    不備修正

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -101,7 +101,7 @@ class Bird(pg.sprite.Sprite):
         引数2 screen：画面Surface
         追加機能1（こうかとんの高速化）
         """
-        print("test")
+        
         sum_mv = [0, 0]
         for k, mv in __class__.delta.items():
             if key_lst[k]:
@@ -388,7 +388,7 @@ def main():
                     score.score_up(-100)
                     bird.change_state("hyper",500)
 
-                if pg.key.get_mods() & pg.KMOD_LSHIFT:  # 追加機能4
+            if pg.key.get_mods() & pg.KMOD_LSHIFT:  # 追加機能4
                     shift_pressed = True
 
 
